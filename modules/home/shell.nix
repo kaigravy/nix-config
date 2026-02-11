99d91a22edf867875355bea32a868e9e41f1013f
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-11 16:55:11 +0000

    fix: ensure systemd works with evict

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.bash = {
     enable = true;
     enableCompletion = true;
+  };
+
+  # Set XDG_CONFIG_HOME for evict's config directory
+  # This is needed for systemd user services to be found
+  home.sessionVariables = {
+    XDG_CONFIG_HOME = "${config.home.homeDirectory}/${config.home.evict.configDirName}";
   };
 }
