3663095347da59f66f8aedcc28bb975fdcff6bf5
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-11 16:57:48 +0000

    fix: systemd evict shenanigans

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -1,5 +1,8 @@
 { config, ... }:
 
+let
+  configDir = "${config.home.homeDirectory}/${config.home.evict.configDirName}";
+in
 {
   programs.bash = {
     enable = true;
@@ -7,8 +10,12 @@
   };
 
   # Set XDG_CONFIG_HOME for evict's config directory
-  # This is needed for systemd user services to be found
   home.sessionVariables = {
-    XDG_CONFIG_HOME = "${config.home.homeDirectory}/${config.home.evict.configDirName}";
+    XDG_CONFIG_HOME = configDir;
+  };
+
+  # Tell systemd where to find user services with evict
+  systemd.user.sessionVariables = {
+    XDG_CONFIG_HOME = configDir;
   };
 }
