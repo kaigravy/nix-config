bb29835da9f4a8f66925d9054c8db31b9fbff0a6
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-16 11:50:38 +0000

    feat: try adding plymouth with a theme

--- modules/nixos/boot.nix
+++ modules/nixos/boot.nix
@@ -6,6 +6,32 @@
     efi.canTouchEfiVariables = true;
   };
 
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "cuts";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["rings"];
+        })
+      ];
+    };
+
+    consoleLogLevel = 3;
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+      "systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+
   # Roll back ephemeral subvolumes to blank snapshots on every boot.
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     mkdir -p /mnt
