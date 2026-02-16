41388eea7828afc3c23deee316cb768aa08fb484
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-16 06:22:09 +0000

    feat: add more fonts

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -2,6 +2,17 @@
 {
     home.packages = with pkgs; [
         nerd-fonts.caskaydia-cove
+        
+        # UI fonts
+        inter
+        cantarell-fonts
+        atkinson-hyperlegible-next
+        
+        # Common word processing fonts
+        liberation_ttf  # Liberation Sans, Serif, Mono (MS Office compatible)
+        dejavu_fonts    # DejaVu Sans, Serif, Mono
+        noto-fonts      # Noto Sans, Serif
+        noto-fonts-emoji
     ];
 
     fonts.fontconfig.enable = true;
