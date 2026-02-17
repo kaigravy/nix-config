27de4660bd48578c9db25a9c66273cd3d50ec178
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-17 12:08:46 +0000

    fix: still trying to fix 'Filen sync service'

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -50,18 +50,21 @@ in
       # Add Doom Emacs to PATH (using actual evict config path)
       export PATH="${configDir}/emacs/bin:$PATH"
     '';
-    
-    # Set XDG_CONFIG_HOME for evict's config directory
-    home.sessionVariables = {
-        XDG_CONFIG_HOME = configDir;
-    };
 
-    # Tell systemd where to find user services with evict
-    systemd.user.sessionVariables = {
-        XDG_CONFIG_HOME = configDir;
-    };
+#    # Set XDG_CONFIG_HOME for evict's config directory
+#    home.sessionVariables = {
+#        XDG_CONFIG_HOME = configDir;
+#    };
+#
+#    # Tell systemd where to find user services with evict
+#    systemd.user.sessionVariables = {
+#        XDG_CONFIG_HOME = configDir;
+#    };
 
-    
+  # Symlink .config to config so systemd can find user services
+  # Systemd looks in ~/.config even when XDG_CONFIG_HOME is set differently
+  home.file.".config".source = config.lib.file.mkOutOfStoreSymlink configDir;
+
     # Spaceship prompt
     plugins = [
       {
