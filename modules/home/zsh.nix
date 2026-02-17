a67ed6aa97d5f159beb3c2861bec9cf443023995
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-17 11:20:12 +0000

    refactor: remove stuff made redudant by evict

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -69,20 +69,6 @@ in
     '';
   };
 
-  # Set XDG_CONFIG_HOME for evict's config directory
-  home.sessionVariables = {
-    XDG_CONFIG_HOME = configDir;
-  };
-
-  # Tell systemd where to find user services with evict
-  systemd.user.sessionVariables = {
-    XDG_CONFIG_HOME = configDir;
-  };
-
-  # Symlink .config to config so systemd can find user services
-  # Systemd looks in ~/.config even when XDG_CONFIG_HOME is set differently
-  home.file.".config".source = config.lib.file.mkOutOfStoreSymlink configDir;
-  
   # Install spaceship prompt
   home.packages = with pkgs; [
     spaceship-prompt
