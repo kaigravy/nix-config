35b3c0902fd150b983706de24dd046e74c6524fb
kaigravy <56808623+kaigravy@users.noreply.github.com>
2026-02-23 09:07:51 +0000

    fix: add necessary env vars for Anki (nixpkgs bug)

--- modules/home/general-programs.nix
+++ modules/home/general-programs.nix
@@ -1,5 +1,24 @@
 { pkgs, lib, config, ... }:
 
+let
+  ankiDataDirs = lib.concatStringsSep ":" [
+    "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}"
+    "${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}"
+    "${pkgs.gtk3}/share"
+    "${pkgs.shared-mime-info}/share"
+  ];
+
+  ankiWrapped = pkgs.symlinkJoin {
+    name = pkgs.anki.name;
+    paths = [ pkgs.anki ];
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram "$out/bin/anki" \
+        --set ANKI_WAYLAND 1 \
+        --prefix XDG_DATA_DIRS : "${ankiDataDirs}"
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     libreoffice-fresh hunspell hunspellDicts.en_AU-large
@@ -29,15 +48,11 @@
     blanket
     wordbook
     gnome-solanum
-    anki
+    ankiWrapped
     gImageReader
     picard
     virtualbox
   ];
-
-  home.sessionVariables = {
-    ANKI_WAYLAND = "1";
-  };
 
   home.persistence."/persist" = lib.mkIf config.home.evict.enable {
     directories = [
