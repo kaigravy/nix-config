051a488df5534392cbad855c3a9da9e2c0d9f6b7
kai <56808623+kaigravy@users.noreply.github.com>
2026-02-23 19:24:12 +1100

    refactor: minor indentation changes

--- modules/home/general-programs.nix
+++ modules/home/general-programs.nix
@@ -1,38 +1,38 @@
 { pkgs, ... }:
 
 {
-    home.packages = with pkgs; [
-        libreoffice-fresh hunspell hunspellDicts.en_AU-large
-        gimp
-        inkscape
-        scribus
-        audacity
-        vlc
-        calibre
-        qbittorrent
-        krita
-        xournalpp
-        obs-studio
-        thunderbird
-        zotero
-        handbrake
-        qalculate-gtk
-        typst typstyle
-        conjure
-        soundconverter
-        timeshift
-        pairdrop
-        rustdesk-flutter
-        zint-qt
-        kdePackages.okular
-        shotcut
-        blanket
-        wordbook
-        gnome-solanum
-        anki
-	gImageReader
-	picard
-	virtualbox
-    ];
+  home.packages = with pkgs; [
+    libreoffice-fresh hunspell hunspellDicts.en_AU-large
+    gimp
+    inkscape
+    scribus
+    audacity
+    vlc
+    calibre
+    qbittorrent
+    krita
+    xournalpp
+    obs-studio
+    thunderbird
+    zotero
+    handbrake
+    qalculate-gtk
+    typst typstyle
+    conjure
+    soundconverter
+    timeshift
+    pairdrop
+    rustdesk-flutter
+    zint-qt
+    kdePackages.okular
+    shotcut
+    blanket
+    wordbook
+    gnome-solanum
+    anki
+    gImageReader
+    picard
+    virtualbox
+  ];
 }
 
